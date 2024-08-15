65fee28ed5a22b2c060b971f759bb7dea8fde352
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2024-08-15 18:39:55 +0200

    change styles of contact list

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -2,15 +2,24 @@ import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { deleteContact } from '../../redux/operations';
 import css from './ContactListItem.module.css';
+import { FaUser } from 'react-icons/fa';
+import { FaPhone } from 'react-icons/fa6';
 
 const ContactListItem = ({ id, name, number }) => {
   const dispatch = useDispatch();
 
   return (
     <li className={css.listItemOfContacts}>
-      <p className={css.nameOfContact}>
-        {name}: {number}
-      </p>
+      <div>
+        <p>
+          <FaUser className={css.iconOfInfo} />
+          {name}
+        </p>
+        <p>
+          <FaPhone className={css.iconOfInfo} />
+          {number}
+        </p>
+      </div>
       <button
         className={css.deleteBtn}
         type="button"
