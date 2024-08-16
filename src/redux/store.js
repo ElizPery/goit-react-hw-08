07370032e5af9d0ea4e115a10b1d3080daeba82f
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2024-08-16 19:53:23 +0200

    fix persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { filterReducer } from "./filters/slice";
 import { contactsReducer } from "./contacts/slice";
@@ -12,11 +21,18 @@ const persistedAuthReducer = persistReducer( {
 }, authReducer)
 
 export const store = configureStore({
-   reducer: {
+  reducer: {
     contacts: contactsReducer,
     filter: filterReducer,
     auth: persistedAuthReducer,
   },
+   middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  // devTools: process.env.NODE_ENV === 'development',
 });
 
 export const persistor = persistStore(store);
