35faca800d688fddbe3a9f540a2a58bb84cbc657
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2024-08-16 10:40:27 +0200

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
 
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-import Loader from './components/Loader/Loader';
-import ContactForm from './components/ContactForm/ContactForm';
-import Filter from './components/Filter/Filter';
-import ContactList from './components/ContactList/ContactList';
+// import Loader from './components/Loader/Loader';
+// import ContactForm from './components/ContactForm/ContactForm';
+// import Filter from './components/Filter/Filter';
+// import ContactList from './components/ContactList/ContactList';
+import HomePage from "./pages/HomePage/HomePage";
 
-import { selectError, selectIsLoading } from './redux/selectors';
-import { fetchContacts } from './redux/operations';
+// import { selectError, selectIsLoading } from './redux/selectors';
+// import { fetchContacts } from './redux/operations';
 
 function App() {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
+  // const dispatch = useDispatch();
+  // const isLoading = useSelector(selectIsLoading);
+  // const error = useSelector(selectError);
 
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(fetchContacts());
+  // }, [dispatch]);
 
   return (
     <div>
-      <h1>Phonebook</h1>
+      {<HomePage />}
+      {/* <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
       <Filter />
       {isLoading && !error && <Loader />}
       {error && Notify.failure('Something went wrong, please try again')}
-      {!isLoading && !error && <ContactList />}
+      {!isLoading && !error && <ContactList />} */}
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { HelmetProvider } from 'react-helmet-async';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -8,7 +9,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
