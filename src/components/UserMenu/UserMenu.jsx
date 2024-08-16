ad794f1a5c79551befb77f7f21d985870f21f5f8
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2024-08-16 22:08:22 +0200

    add handling loading and error situations while logout

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,18 +1,36 @@
-import { useDispatch, useSelector } from "react-redux";
-import { selectUser } from "../../redux/auth/selectors";
-import css from "./UserMenu.module.css"
-import { logOut } from "../../redux/auth/operations";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectUser,
+  selectError,
+  selectIsLoading,
+} from '../../redux/auth/selectors';
+import Loader from '../../components/Loader/Loader';
+import css from './UserMenu.module.css';
+import { logOut } from '../../redux/auth/operations';
 
 const UserMenu = () => {
   const user = useSelector(selectUser);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
   const dispatch = useDispatch();
 
   return (
     <div className={css.wrapper}>
-      <p className={css.username}>Welcome, {user.name}</p>
-      <button type="button" onClick={() => dispatch(logOut())}>Logout</button>
+      {isLoading && !error && <Loader />}
+      {error && Notify.failure(`Something went wrong, please try again!`)}
+      {!isLoading && !error && (
+        <>
+          <p className={css.username}>Welcome, {user.name}</p>
+          <button type="button" onClick={() => dispatch(logOut())}>
+            Logout
+          </button>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default UserMenu;
