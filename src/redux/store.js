908bbb220fe517892cee9faba14177336c4d8e88
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2024-08-16 18:29:04 +0200

    add persist for token

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { filterReducer } from "./filters/slice";
 import { contactsReducer } from "./contacts/slice";
-import authReducer from "./auth/slice"
+import authReducer from "./auth/slice";
 
+const persistedAuthReducer = persistReducer( {
+  key: 'auth-token',
+  storage,
+  whitelist: ["token"],
+}, authReducer)
 
 export const store = configureStore({
    reducer: {
     contacts: contactsReducer,
     filter: filterReducer,
-    auth: authReducer,
+    auth: persistedAuthReducer,
   },
 });
+
+export const persistor = persistStore(store);
